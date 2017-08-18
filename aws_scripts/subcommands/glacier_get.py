7a7eb1cbbd49835f13a294ea15a6daf15f76af1b
Sheena Todhunter <sheenams@uw.edu>
2017-08-18 14:46:50 -0700

    add wait statement to glacier_get

--- aws_scripts/subcommands/glacier_get.py
+++ aws_scripts/subcommands/glacier_get.py
@@ -15,6 +15,7 @@ import sys
 import os.path
 import json
 import logging
+import time
 from datetime import date
 
 fname = __name__.split('.')[-1] + '.log'
@@ -37,11 +38,15 @@ def build_parser(parser):
 
 def action(args):
     target_vault_name = args.target_vault_name
-    archived_id = args.archive_id
+    archive_id = args.archive_id
 
+    print target_vault_name, archive_id
     glacier = boto.glacier.connect_to_region('us-west-2')
     vault = glacier.get_vault(target_vault_name)
     job = vault.retrieve_archive(archive_id)
+    while not job.completed:
+        time.sleep(10)
+
     job.download_to_file(args.archive_filename + 'tar.gz')
 
     print("Operation complete.")
