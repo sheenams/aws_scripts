2f76734e324a445a144a363b2d109e7348448d96
Sheena Todhunter <sheenams@uw.edu>
2019-07-30 17:09:50 -0700

    add multi string split, remove assay from command input

--- aws_scripts/subcommands/s3_sync.py
+++ aws_scripts/subcommands/s3_sync.py
@@ -12,6 +12,7 @@ import os
 import subprocess
 import logging
 import glob
+import sys
 
 #Copy archive folder to S3 bucket
 #Use ngs_upload_user credentials
@@ -23,9 +24,27 @@ def build_parser(parser):
                         help ='type of sequencing performed, which maps to s3 bucket name')
     parser.add_argument('year', type = check_year, 
                         help ='year the data was created')
-    parser.add_argument('assay', type=str,
-                        choices=['opx','bro','imd','epi','mrw','glt','sth','hh','npm1','pdnas','msi','monc'],
-                        help='Optional assay parameter in case folder name is not of controlled format')
+
+def multi_split(source, splitlist):
+    """
+    Function to split a string given a string of multiple split points
+    """
+    output = []
+    atsplit = True
+    if source is None:
+        return None
+    else:
+        for char in source:
+            if char in splitlist:
+                atsplit = True
+            else:
+                if atsplit:
+                    output.append(char)
+                    atsplit = False
+                else:
+                    output[-1] = output[-1] + char
+    return output
+
 
 def define_platform(platform):
     """This function maps the initials of the command line arguement
@@ -35,7 +54,17 @@ def define_platform(platform):
                'wgs':'wgs'}
     return platforms[platform]
 
+def define_assay(project):
+    """Define assay from the project name """
+    project_items=multi_split(project, '_-')
+    if len(project_items)>=4:
+        assay=project_items[3].split('v')[0]
+    else:
+        assay='error in parsing assay name from project'
 
+    return assay
+
+    
 def check_year(value):
     """Validate that the year provided as an argument is YYYY format"""
     if len(value) !=4 :
@@ -47,7 +76,7 @@ def setup_file_structures(folder, filetype, platform,assay_name,year,project):
     cmds=[]
     if os.path.exists(folder):
         if 'settings' in folder:
-            cmds.append(["aws", "s3", "cp", "--recursive",  folder,  "s3://uwlm-ngs-data/{platform}/{assay_name}/analysis_files/{year}/{project}/project/settings_files/".format(platform=platform, assay_name=assay_name, year=year, project=project)])
+            cmds.append(["aws", "s3", "cp", "--recursive", folder, "s3://uwlm-ngs-data/{platform}/{assay_name}/analysis_files/{year}/{project}/project/settings_files/".format(platform=platform, assay_name=assay_name, year=year, project=project)])
         else:
             for filename in os.listdir(folder):
                 #Decide if we're working with Analysis subdirs or individual files
@@ -64,8 +93,8 @@ def setup_file_structures(folder, filetype, platform,assay_name,year,project):
                     sampletype='project'
                 else:
                     sampletype='samples'
-                cmds.append(aws_cmd+[fname,  "s3://uwlm-ngs-data/{platform}/{assay_name}/{filetype}/{year}/{project}/{sampletype}/{sampleid}/".format(sampleid=sampleid,platform=platform, assay_name=assay_name, year=year, project=project, filetype=filetype, sampletype=sampletype)])
-        #print([x for x in cmds])
+                cmds.append(aws_cmd+[fname, "s3://uwlm-ngs-data/{platform}/{assay_name}/{filetype}/{year}/{project}/{sampletype}/{sampleid}/".format(sampleid=sampleid,platform=platform, assay_name=assay_name, year=year, project=project, filetype=filetype, sampletype=sampletype)])
+#        print([x for x in cmds])
     for cmd in cmds:
         count=0
         exitcode=subprocess.call(cmd)
@@ -83,8 +112,7 @@ def action(args):
     platform=define_platform(args.platform)
     project=os.path.basename(args.folder.strip('/'))
     year=args.year
-    assay_name=args.assay
-
+    assay_name=define_assay(project)
 
     #Create folder variables
     #{platform}/{assay_name}/fastqs/{YYYY}/{project_name}/{samples}|{qc_controls}
@@ -101,9 +129,9 @@ def action(args):
     analysis_files=setup_file_structures(analysis_folder,'analysis_files', platform, assay_name, year, project)
     #Settings files goes under 'analysis_files' folder in its own special "sampletype" folder
     settings_files=setup_file_structures(settings_folder,'analysis_files', platform, assay_name, year, project)
-    print("{} upload complete".format(project))
-    #List what is in s3 and compare to what was expected
-    aws_files=report_uploads()
+    # print("{} upload complete".format(project))
+    # #List what is in s3 and compare to what was expected
+    # aws_files=report_uploads()
     
     
     
